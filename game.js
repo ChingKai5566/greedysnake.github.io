29b08c07dedf9e515ce30d8a1871954a54e3ffc5
ChingKai5566 <chingkai.hung5566@gamil.com>
2020-07-29 17:20:23 +0800

    xzz

---
+++ game.js
@@ -0,0 +1,48 @@
+import {
+	SNAKE_SPEED,
+	update as updateSnake,
+	draw as drawSnake,
+	getSnakeHead,
+	snakeIntersection,
+} from './snake.js';
+
+import { update as updateFood, draw as drawFood } from './food.js';
+import { outSideGrid } from './grid.js';
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
+
+function main(currentTime) {
+	if (gameOver) {
+		alert('Loser');
+		return;
+	}
+
+	window.requestAnimationFrame(main);
+	const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
+	if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
+
+	lastRenderTime = currentTime;
+
+	update();
+	draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+	updateSnake();
+	updateFood();
+	checkDeath();
+}
+
+function draw() {
+	gameBoard.innerHTML = '';
+	drawSnake(gameBoard);
+	drawFood(gameBoard);
+}
+
+function checkDeath() {
+	gameOver = outSideGrid(getSnakeHead()) || snakeIntersection();
+}
